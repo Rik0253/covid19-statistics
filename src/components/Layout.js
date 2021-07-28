b5e8b6327d19938b41ce1841e07cc5b7940827da
rik0253 <das.sidd89@gmail.com>
2021-07-28 22:26:26 +0530

    Rectified the issue

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,7 +8,7 @@ export default class Layout extends Component {
                 <p className="lg:text-6xl sm:text-1xl text-center p-2 justify-items-center">
                     {
                         (this.props.data.img)?
-                        <img className="object-contain rounded lg:h-8 md:h-2 inline-block" src={this.props.data.img} alt="country image" />
+                        <img className="object-contain rounded lg:h-8 md:h-2 inline-block" src={this.props.data.img} alt="country" />
                         :
                         ''
                     }  {this.props.data.text}
