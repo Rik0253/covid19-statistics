956a2ecb36e3ba6aeb221a665d8f7506d26e8c91
rik0253 <das.sidd89@gmail.com>
2021-08-04 22:45:08 +0530

    Rectified the issue

--- src/components/SpecificCountry/ChartsData.js
+++ src/components/SpecificCountry/ChartsData.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import axios from "axios";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Label } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import {format} from 'date-fns';
 
 
